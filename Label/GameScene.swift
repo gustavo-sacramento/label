d5e9befe13ba38e6f5fe6b9a75937444e1762fab
Gustavo Sacramento <gustavo.saramento211@gmail.com>
2023-03-21 09:57:50 -0300

    Starting to use movement and actions, including action sequences.

--- Label/GameScene.swift
+++ Label/GameScene.swift
@@ -11,11 +11,28 @@ class GameScene: SKScene {
     let label = SKLabelNode(text: "Hello SpriteKit!")
     
     override func didMove(to view: SKView) {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
         label.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
         label.fontSize = 45
         label.fontColor = SKColor.cyan
         label.fontName = "Avenir"
         
         addChild(label)
+        view.addGestureRecognizer(recognizer)
+    }
+    
+    @objc func tap(recognizer: UIGestureRecognizer) {
+        let viewLocation = recognizer.location(in: view)
+        let sceneLocation = convertPoint(fromView: viewLocation)
+        let moveByAction = SKAction.moveBy(x: sceneLocation.x - label.position.x, y: sceneLocation.y - label.position.y, duration: 1)
+        
+        // Reversed action
+        let moveByReversedAction = moveByAction.reversed()
+        let moveByActions = [moveByAction, moveByReversedAction]
+        let moveSequence = SKAction.sequence(moveByActions)
+        let moveRepeatSequence = SKAction.repeat(moveSequence, count: 3)
+        let moveRepeatForeverSequence = SKAction.repeatForever(moveSequence)
+    
+        label.run(moveRepeatForeverSequence)
     }
 }
